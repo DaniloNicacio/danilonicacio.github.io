11cf3eb17b7c8221142c8956980b6034b4ee24d8
DaniloNicacio <danilonicacio2016@gmail.com>
2023-12-04 21:59:58 -0400

    [FEAT] - Added profile picture and placeholder

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Image from "next/image";
 const HeroSection = () =>{
     return (
         <section>
@@ -12,7 +12,17 @@ const HeroSection = () =>{
                         I'm just a placeholder :D
                     </p>
                 </div>
-                <div className="col-span-5"></div>
+                <div className="col-span-5 place-self-center">
+                    <div className="rounded-full bg-[#181818] w-[250px] h-[250px]  lg:w-[400px] lg:h-[400px] relative">
+                        <Image
+                            src="/images/hero-image.jpg"
+                            alt="hero image"
+                            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                            width={300}
+                            height={300}
+                        />
+                    </div>
+                </div>
             </div>
         </section>
     );
