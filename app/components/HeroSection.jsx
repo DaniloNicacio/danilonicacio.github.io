7186b334459ce41cc0a1ac5138f354f3373e8cd5
DaniloNicacio <danilonicacio2016@gmail.com>
2023-12-04 23:26:58 -0400

    [FEAT] - Increment Style

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -4,17 +4,24 @@ const HeroSection = () =>{
     return (
         <section>
             {/* Presentation Setup */}
-            <div className="grid grid-cols-1 lg:grid-cols-12">
-                <div className="col-span-7 place-self-center">
-                    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-                        Hello, I'm Danilo!
+            <div className="grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-7 place-self-center text-center sm:text-left">
+                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-blue-300">
+                            Hello, I'm{" "}
+                        </span>
+                        Danilo
                     </h1>
-                    <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
+                    <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
                         I'm just a placeholder :D
                     </p>
                     <div>
-                        <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">Hire Me</button>
-                        <button className="px-6 py-3 rounded-full mr-4 bg-transparent hover:bg-slate-800 text-white border border-white mt-3">Download CV</button>
+                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+                            Hire Me
+                        </button>
+                        <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br hover:bg-slate-800 text-white border mt-3">
+                            <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
+                        </button>
                     </div>
                 </div>
                 {/* Profile Picture Setup */}
