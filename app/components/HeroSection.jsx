f4c7c458daaac89e34cd431a7b5ef3b4b0626fde
DaniloNicacio <danilonicacio2016@gmail.com>
2023-12-04 20:48:07 -0400

    [FEAT] - Header and placeholder added

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeroSection = () =>{
+    return (
+        <section>
+            <h1 className="text-white">Hello, I'm Danilo!</h1>
+            <p className="">
+                I'm just a placeholder :D
+            </p>
+        </section>
+    );
+};
+
+export default HeroSection;
