b09c62dad0914a58a3d24a0a1bf6aa7a7ab23f35
DaniloNicacio <danilonicacio2016@gmail.com>
2023-12-11 18:54:35 -0400

    [FEAT] - Added navbar button for mobile screens

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+"use client";
+import React, {useState} from "react";
 import Link from "next/link";
 import NavLink from "@/app/components/NavLink";
+import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
 
 {/* Navbar Setup */}
 const navLinks = [
@@ -22,10 +24,23 @@ const navLinks = [
     }
 ]
 const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false);
     return(
-        <nav>
-            <div className="flex flex-wrap items-center justify-between mx-auto p-8">
+        <nav className="fixed top-0 left-0 right-10 z-10 bg-[#121212] bg-opacity-90">
+            <div className="flex flex-wrap items-center justify-between mx-auto px-4">
                 <Link href={'/'} className="text-lg md:text-5xl text-white font-semibold">LOGO</Link>
+                <div className="mobile-menu block md:hidden">
+                    <button
+                        onClick={() => setNavbarOpen(!navbarOpen)}
+                        className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+                    >
+                        {navbarOpen ? (
+                            <XMarkIcon className="h-5 w-5" />
+                        ) : (
+                            <Bars3Icon className="h-5 w-5" />
+                        )}
+                    </button>
+                </div>
                 <div className="menu hidden md:block md:w-auto" id="navbar">
                     <ul className="flex p-4 md:pd-0 md:flex-row md:space-x-8 mt-0">
                         {
