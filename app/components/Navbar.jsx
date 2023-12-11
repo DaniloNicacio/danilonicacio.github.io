a99cb741eec517ba1ff30196519cc0025ea8d0ed
DaniloNicacio <danilonicacio2016@gmail.com>
2023-12-11 19:13:09 -0400

    [FEAT] - Display navbar into button

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 import Link from "next/link";
 import NavLink from "@/app/components/NavLink";
 import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
+import MenuOverlay from "@/app/components/MenuOverlay";
 
 {/* Navbar Setup */}
 const navLinks = [
@@ -53,6 +54,7 @@ const Navbar = () => {
                     </ul>
                 </div>
             </div>
+            {navbarOpen ? <MenuOverlay links = {navLinks}/> : null}
         </nav>
     );
 };
