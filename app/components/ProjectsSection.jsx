b47e5912aa35fba4e634bfb55bb215c6388a2e55
DaniloNicacio <danilonicacio2016@gmail.com>
2023-12-29 15:16:38 -0400

    [FEAT] - Added All and Web buttons above projects section

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -7,7 +7,9 @@ const projectsData = [
         title: "Work in Progress",
         description: "This is just a placeholder",
         image: "/images/projects/work-in-progress.png",
-        tag: ["All", "Web"]
+        tag: ["All", "Web"],
+        gitUrl: "/",
+        previewUrl: "/"
     }
 ]
 
@@ -17,6 +19,14 @@ const ProjectsSection = () =>{
             <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
                 My Projects
             </h2>
+            <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+                <button className="rounded-full border-2 border-red-500 px-6 py-3 text-xl cursor-pointer">
+                    All
+                </button>
+                <button className="rounded-full border-2 border-slate-600 hover:border-white px-6 py-3 text-xl cursor-pointer">
+                    Web
+                </button>
+            </div>
             <div className="grid md:grid-cols-3 gab-8 md:gap-12">
                 {projectsData.map((project) => (
                 <ProjectCard
@@ -25,6 +35,8 @@ const ProjectsSection = () =>{
                     description={project.description}
                     imgUrl={project.image}
                     tags={project}
+                    gitUrl={project.gitUrl}
+                    previewUrl={project.previewUrl}
                 />
             ))}
             </div>
