18734ea55a5817b26c8529534b2648fd23af299a
DaniloNicacio <danilonicacio2016@gmail.com>
2023-12-29 16:04:29 -0400

    [FEAT] - Create a ProjectTag component and implements a filter in ProjectsSection

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -1,5 +1,7 @@
-import React from "react"
+"use client";
+import {React, useState} from "react"
 import ProjectCard from "@/app/components/ProjectCard";
+import ProjectTag from "@/app/components/ProjectTag";
 
 const projectsData = [
     {
@@ -14,21 +16,34 @@ const projectsData = [
 ]
 
 const ProjectsSection = () =>{
+    const [tag, setTag] = useState("All");
+    const handleTagChange = (newTag) =>{
+        setTag(newTag)
+    }
+
+    const filteredProjects = projectsData.filter((project) =>
+        project.tag.includes(tag)
+     )
+
     return(
         <>
             <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
                 My Projects
             </h2>
             <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
-                <button className="rounded-full border-2 border-red-500 px-6 py-3 text-xl cursor-pointer">
-                    All
-                </button>
-                <button className="rounded-full border-2 border-slate-600 hover:border-white px-6 py-3 text-xl cursor-pointer">
-                    Web
-                </button>
+                <ProjectTag
+                    onClick={handleTagChange}
+                    name="All"
+                    isSelected={tag === "All"}
+                />
+                <ProjectTag
+                    onClick={handleTagChange}
+                    name="Web"
+                    isSelected={tag === "Web"}
+                />
             </div>
             <div className="grid md:grid-cols-3 gab-8 md:gap-12">
-                {projectsData.map((project) => (
+                {filteredProjects.map((project) => (
                 <ProjectCard
                     key={project.id}
                     title={project.title}
