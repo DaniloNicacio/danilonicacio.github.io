f15f509fe51a8c4a32a74ba3735dbc86ee6fa016
DaniloNicacio <danilonicacio2016@gmail.com>
2024-02-07 19:51:58 -0400

    [REFACTOR] - Replace red with primary color and update tailwind config

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -9,8 +9,8 @@ const HeroSection = () =>{
             {/* Presentation Setup */}
             <div className="grid grid-cols-1 sm:grid-cols-12">
                 <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
-                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
-                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-blue-300">
+                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-600 to-blue-300">
                             Hello, I&apos;m{" "}
                         </span>
                         <br/>
