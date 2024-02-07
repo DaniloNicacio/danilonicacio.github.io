be61adade7fd62de398b9ef5c8e2b50fea631e53
DaniloNicacio <danilonicacio2016@gmail.com>
2024-02-07 19:30:55 -0400

    [REFACTOR/FEAT] - Refactor About Section, change exhibition dynamic to static and implement form to send emails

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -1,15 +1,50 @@
-import React from "react";
+"use client";
+import React, {useState} from "react";
 import GithubIcon from "../../public/github-icon.svg"
 import LinkedinIcon from "../../public/linkedin-icon.svg"
 import Link from "next/link"
 import Image from "next/image"
 
 const EmailSection = () => {
+    const [emailSubmitted, setEmailSubmitted] = useState(false)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            email: e.target.email.value,
+            subject: e.target.subject.value,
+            message: e.target.message.value,
+        };
+        const JSONdata = JSON.stringify(data)
+        const endpoint = "/api/send"
+
+        // Form the request for sending data to the server.
+        const options = {
+            // The method is POST because we are sending data.
+            method: "POST",
+            // Tell the server we're sending JSON.
+            headers: {
+                "Content-Type": "application/json",
+            },
+            // Body of the request is the JSON data we created above.
+            body: JSONdata,
+        }
+
+        const response = await fetch(endpoint, options)
+        const resData = await response.json()
+
+        if (response.status === 200) {
+            console.log("Message sent.")
+            setEmailSubmitted(true)
+        }
+    }
+
     return (<section className = "grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
             <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-red-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
         <div className="z-10">
             <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
+            {" "}
             I'm currently looking for new opportunities, my inbox is always open.
             Whether you have a question or just want to say hi, I'll try my best
             to get back to you!
@@ -24,7 +59,7 @@ const EmailSection = () => {
             </div>
         </div>
         <div>
-            <form className="flex flex-col">
+            <form className="flex flex-col" onSubmit={handleSubmit}>
                 <div className="mb-6">
                     <label
                         htmlFor="email"
@@ -32,6 +67,7 @@ const EmailSection = () => {
                         Your email
                     </label>
                     <input
+                        name="email"
                         type="email"
                         id="email"
                         required
@@ -46,6 +82,7 @@ const EmailSection = () => {
                         Subject
                     </label>
                     <input
+                        name="subject"
                         type="text"
                         id="subject"
                         required
