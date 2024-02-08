9c1ef7699705e5aaa10ba41f3d94b3c65fa84164
DaniloNicacio <danilonicacio2016@gmail.com>
2024-02-08 19:01:09 -0400

    [FEAT] - Add animations to achievementssection

--- app/components/AchievementsSection.jsx
+++ app/components/AchievementsSection.jsx
@@ -1,4 +1,11 @@
+"use client"
 import React from "react"
+import dynamic from "next/dynamic";
+
+const AnimatedNumbers = dynamic(() => {
+    return import ("react-animated-numbers")
+}, {ssr: false}
+    )
 
 const achievementsList = [
     {
@@ -7,25 +14,43 @@ const achievementsList = [
     },
     {
         metric: "Years Programming",
-        value: "5+",
+        value: "5",
+        postfix: "+",
     },
     {
         metric: "Professional Experience",
-        value: "1Y",
+        value: "1",
+        postfix: "Y",
     },
 ]
 
 const AchievementsSection = () =>{
     return (
         <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-            <div className="border-[#33353F] border rounded-md py-8 px-17 flex flex-row items-center justify-between">
+            <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
             {achievementsList.map((achievement, index) => {
               return (
                   <div key={index} className="flex flex-col items-center justify-center mx-4">
+                      {achievement.prefix}
                       <h2 className="text-white text-4xl font-bold">
-                          {achievement.value}</h2>
+                          <AnimatedNumbers
+                              includeComma
+                              animateToNumber={parseInt(achievement.value)}
+                              locale="en-US"
+                              className="text-white text-4xl font-bold"
+                              configs={((_, index) => {
+                                  return {
+                                      mass: 1,
+                                      friction: 100,
+                                      tensions: 140 * (index + 1),
+                                  }
+                              })}
+                          />
+                          {achievement.prefix}
+                      </h2>
                       <p className="text-[#ADB7BE] text-base">
-                          {achievement.metric}</p>
+                          {achievement.metric}
+                      </p>
                   </div>
               )
             })}
