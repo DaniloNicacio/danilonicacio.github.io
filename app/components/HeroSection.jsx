fc70f93a66582e4f524f751cb712e3f3f366ad49
DaniloNicacio <danilonicacio2016@gmail.com>
2024-02-13 01:22:28 -0400

    [FIX] - Fix Hello animation on mobile

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -37,7 +37,7 @@ const HeroSection = () => {
                     initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.5 }}
-                    className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+                    className="col-span-8 place-self-center text-center sm:text-center justify-self-start"
                 >
                     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-blue-300">
